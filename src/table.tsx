5655fa9c5f7b9569f0c2faaa6a91e71197a6a128
Bartosz Szar <szarbartosz@gmail.com>
2024-02-15 21:38:31 +0100

    add filtering and table loading indication

--- src/table.tsx
+++ src/table.tsx
@@ -5,13 +5,14 @@ export default function Command() {
   const standings = useTable();
 
   return (
-    <List>
+    <List throttle isLoading={!standings}>
       {standings?.map((team) => (
         <List.Item
           key={team.position}
           icon={team.logoUrl}
           title={team.position.toString()}
           subtitle={team.teamName}
+          keywords={[team.teamName]}
           accessories={[{ icon: Icon.Dot, text: team.teamPoints.toString() }]}
           actions={
             <ActionPanel>
