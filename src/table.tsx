23fe85921ebdf3159c962b2d7d335f181a24e29a
Bartosz Szar <szarbartosz@gmail.com>
2024-02-16 00:22:11 +0100

    add tooltips

--- src/table.tsx
+++ src/table.tsx
@@ -4,6 +4,10 @@ import useTable from "./hooks/useTable";
 export default function Command() {
   const standings = useTable();
 
+  // const accessories = useMemo(() => {
+  //   standings?.map((team) => [
+  // }, []);
+
   return (
     <List throttle isLoading={!standings}>
       {standings?.map((team) => (
@@ -14,15 +18,20 @@ export default function Command() {
           subtitle={team.teamName}
           keywords={[team.teamName]}
           accessories={[
-            { icon: Icon.SoccerBall, text: team.gamesPlayed.toString() },
-            { icon: Icon.Goal, text: `${team.goalsFor.toString()} - ${team.goalsAgainst.toString()}` },
-            ...team.lastResults.reverse().map((result) => ({
+            { icon: Icon.SoccerBall, text: team.gamesPlayed.toString(), tooltip: "Games Played" },
+            {
+              icon: Icon.Goal,
+              text: `${team.goalsFor.toString()} - ${team.goalsAgainst.toString()}`,
+              tooltip: "Goals For - Goals Against",
+            },
+            ...team.lastResults.map((result) => ({
               icon: {
                 source: Icon.CircleFilled,
                 tintColor: result === 0 ? Color.Red : result === 1 ? Color.SecondaryText : Color.Green,
               },
+              tooltip: result === 0 ? "Lost" : result === 1 ? "Draw" : "Won",
             })),
-            { icon: Icon.LineChart, text: team.teamPoints.toString() },
+            { icon: Icon.Trophy, text: team.teamPoints.toString(), tooltip: "Points" },
           ]}
           actions={
             <ActionPanel>
